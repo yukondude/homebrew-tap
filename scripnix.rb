8432fd50976338606be050cb18ee1f69a093fa71
Dave Rogers <thedude@yukondude.com>
2016-10-28 15:57:30 -0700

    Version bumped to 0.1.9.

--- scripnix.rb
+++ scripnix.rb
@@ -1,9 +1,9 @@
 class Scripnix < Formula
   desc "Useful Python3 and bash shell scripts for macOS/BSD and *NIX. Useful to me, at any rate. YMMV."
   homepage "https://yukondude.github.io/Scripnix/"
-  url "https://github.com/yukondude/Scripnix/archive/0.1.8.tar.gz"
-  version "0.1.8"
-  sha256 "c488af9f3e5011713a3f02eae9fa3db4e56f22adccf6431e44aec33f67634618"
+  url "https://github.com/yukondude/Scripnix/archive/0.1.9.tar.gz"
+  version "0.1.9"
+  sha256 "04702c23142ce8e1335d1d47ac5e55f237396a2fd0c30275599bd910aeaca511"
 
   depends_on :python3
   depends_on "coreutils"
