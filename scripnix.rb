645baa3997d712ee2c809f83041a6783570c6cb5
Dave Rogers <thedude@yukondude.com>
2016-11-10 14:59:10 -0800

    Bumped to 0.1.10 and added gnu-sed dependency.

--- scripnix.rb
+++ scripnix.rb
@@ -1,12 +1,13 @@
 class Scripnix < Formula
   desc "Useful Python3 and bash shell scripts for macOS/BSD and *NIX. Useful to me, at any rate. YMMV."
   homepage "https://yukondude.github.io/Scripnix/"
-  url "https://github.com/yukondude/Scripnix/archive/0.1.9.tar.gz"
-  version "0.1.9"
-  sha256 "04702c23142ce8e1335d1d47ac5e55f237396a2fd0c30275599bd910aeaca511"
+  url "https://github.com/yukondude/Scripnix/archive/0.1.10.tar.gz"
+  version "0.1.10"
+  sha256 "f272c73117208a017c1e8fc75cdc1a6575020a059c15b647c45f1e34701a81e7"
 
   depends_on :python3
   depends_on "coreutils"
+  depends_on "gnu-sed"
 
   def install
     ENV["PYTHON"] = "python3"
